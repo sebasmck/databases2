8849124dbcc5bf50bfb6e2634efc00772fc806da
sebasmck <corporate.att@gmail.com>
2018-03-27 12:10:05 -0500

    Updated Models and Bulma.io Added to project.
    execute : composer update to see changes on app.

---
+++ project/app/City.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    
+    protected $table = '';
+
+    protected $primaryKey = '';
+
+    protected $fillable = [];
+
+    public $timestamps = false;
+
+
+}
---
+++ project/app/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    protected $table = '';
+
+    protected $primaryKey = '';
+
+    protected $fillable = [];
+
+    public $timestamps = false;
+}
